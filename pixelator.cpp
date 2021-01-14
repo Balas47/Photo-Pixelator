a13dbbf27d3f1089ade04747bbf1c39223347426
Balas47 <brianalas01@gmail.com>
2021-01-14 14:16:22 -0500

    Implementation of a blur filter to help with pixelation

--- pixelator.cpp
+++ pixelator.cpp
@@ -1,14 +1,68 @@
 #define cimg_display 0
+#define debug
 
 #include "CImg.h"
 #include <iostream>
 #include <Eigen/Dense>
 using namespace cimg_library;
 
+const int MIN_IN = 3;
+const double FILTER = 9.0;
+
+// Simple 3x3 blur filter
+void blurFilter(Eigen::Vector3d *&image, int height, int width){
+
+    Eigen::Vector3d *avg = image;
+    image = new Eigen::Vector3d[height*width];
+
+    for(unsigned int i=0;i<height;i++){
+        for(unsigned int j=0;j<width;j++){
+
+            Eigen::Vector3d pixlavg = avg[i*width+j];
+
+            // Get information from the pixles above if possible
+            if(i > 0){
+                pixlavg += avg[(i-1)*width+j];
+                if(j > 0) pixlavg += avg[(i-1)*width+(j-1)];
+                if(j < (width-1)) pixlavg += avg[(i-1)*width+(j+1)];
+            }
+
+            //Get information from the pixels in the current row if possible              
+            if(j > 0) pixlavg += avg[i*width+(j-1)];
+            if(j < (width-1)) pixlavg += avg[i*width+(j+1)];
+
+            // Get information from the pixels above if possible
+            if(i < (height-1)){
+                pixlavg += avg[(i+1)*width+j];
+                if(j > 0) pixlavg += avg[(i+1)*width+(j-1)];
+                if(j < (width-1)) pixlavg += avg[(i+1)*width+(j+1)];
+            }
+
+            pixlavg /= FILTER;
+            image[i*width+j] = pixlavg;
+
+        }
+    }
+
+    delete[] avg;
+
+}
+
 int main(int argc, char *argv[]){
 
+    char *inFile, *outFile;
+
+    // Storing all input into appropriate variables
+    if(argc < MIN_IN){
+        std::cout << "NOT ENOUGH ARGUMENTS!" << std::endl;
+        return 1;
+    }else{
+        inFile = argv[1];
+        outFile = argv[2];
+    }
+
     // Read in the base image and store its contents in a vector to manipulate later
-    CImg<double> input(argv[1]);
+    CImg<double> input(inFile);
     Eigen::Vector3d *image = new Eigen::Vector3d[input.height()*input.width()];
     for (unsigned int i=0; i<input.height(); i++) {
         for (unsigned int j=0; j<input.width(); j++) {
@@ -17,6 +71,16 @@ int main(int argc, char *argv[]){
 	        image[i*input.width()+j][2] = input(j, i, 2);
         }
     }
+
+    // Apply the simple blur filter
+    blurFilter(image, input.height(), input.width());
+
+#ifdef debug
+    // Just for testing, apply the filter 10 times
+    for(int i=0;i<10;i++){
+        blurFilter(image, input.height(), input.width());
+    }
+#endif
 
     // Save the image information appropriately for output
     CImg<double> output(input.width(), input.height(), input.depth(), input.spectrum(), 0);
@@ -29,10 +93,10 @@ int main(int argc, char *argv[]){
     }
 
     // Save the image in the appropriate format (just png, jpg for now)
-    if (strstr(argv[2], "png"))
-	    output.save_png(argv[2]);
-    else if (strstr(argv[2], "jpg"))
-	    output.save_jpeg(argv[2]);
+    if (strstr(outFile, "png"))
+	    output.save_png(outFile);
+    else if (strstr(outFile, "jpg"))
+	    output.save_jpeg(outFile);
 
     delete[] image;
 
